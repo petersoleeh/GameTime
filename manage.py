26ff42230c17c4075778c8a56f0570c05749630d
peter soleeh <petersoleeh@gmail.com>
2017-11-06 16:18:11 +0300

    add shell

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 
 #creating app instance
@@ -6,6 +6,12 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db )
+
 
 if __name__ == '__main__':
     manager.run()
