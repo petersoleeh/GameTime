6bbcd3e4d9e200d88100bc4a8b7dff5406c08adf
David Mwangi <njorogedavid13@gmail.com>
2017-11-06 17:25:35 +0300

    add static files to index

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+
+
+
+
+
+
+
+
+@main.route('/')
+def index():
+    """
+    view function for the landing page
+    """
+    return render_template('index.html')
