4cf2fc416ce2d2de2879d986d8394ea46572affd
peter soleeh <petersoleeh@gmail.com>
2017-11-07 14:44:56 +0300

    change db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://soleeh:soleeh..@localhost/gametime'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://soleeh:soleeh..@localhost/gametime'
     GAME_WEEK_API='https://gtime-api.herokuapp.com/teams/week/12'
 
 
@@ -26,6 +26,9 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://soleeh:soleeh..@localhost/gametime'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///game_time.db'
 
     DEBUG = True
 
