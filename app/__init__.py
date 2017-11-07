a6c62f164cf462bedcee357ab5fa9b7b4bbf7b9a
peter soleeh <petersoleeh@gmail.com>
2017-11-07 12:14:01 +0300

    small update

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,7 @@ def create_app(config_name):
     login_manager.init_app(app)
 
 
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
