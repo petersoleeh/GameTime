54fb80350af0ece9d6e9414163758a9dd4c9134f
David Mwangi <njorogedavid13@gmail.com>
2017-11-08 17:29:46 +0300

    allow only authenticated users to add to favourites

--- app/main/views.py
+++ app/main/views.py
@@ -5,27 +5,25 @@ from ..models import Favourite,User
 from flask_login import login_required,login_user, current_user
 
 
-
-
-
-
-
-
 @main.route('/', methods = ["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        team_id = request.form.get('add_f')
-        print(team_id)
-        favourite_team = Favourite(team_id = team_id, user = current_user)
-        favourite_team.add_favorites()
-        return redirect ('/')
     """
     view function for the landing page
     """
+    favourites=[]
     week_fixture=get_week()
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            team_id = request.form.get('add_f')
+            print(team_id)
 
-    favourites=Favourite.query.filter_by(user=current_user)
-    # print(len(week_fixture))
+            favourite_team = Favourite(team_id = team_id, user = current_user)
+            favourite_team.add_favorites()
+            return redirect ('/')
+
+        favourites=Favourite.query.filter_by(user=current_user)
+        # print(len(week_fixture))
+        return render_template('index.html',week_fixture=week_fixture,favourites=favourites)
     return render_template('index.html',week_fixture=week_fixture,favourites=favourites)
 
 
